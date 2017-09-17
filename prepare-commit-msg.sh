6fadb0bf5bf6dcb4033104da5e309206c15bbf0b
Samuel Huang <samhuang91@gmail.com>
2017-09-17 09:12:00 -0700

    Refine comments in script"

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -5,7 +5,7 @@ source "${GIT_DIR}/hooks/commons"
 replace_tag() {
     git log >& /dev/null || return
 
-    # utilize branch prefix as commit-tag for message
+    # Utilize branch prefix as commit-tag for message
     CURRENT=$(git rev-parse --abbrev-ref HEAD)
     CBRANCH=$(echo "${CURRENT}" \
         | awk -F '/' '{print $1}' \
@@ -14,7 +14,6 @@ replace_tag() {
 }
 
 # Resolve merge, message and template
-
 case ":$2" in
     :message)
         replace_tag "$1" ;;
