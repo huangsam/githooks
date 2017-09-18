d1b3506d1d6e4d05628b7748469a27d52cc26483
Samuel Huang <samhuang91@gmail.com>
2017-09-17 18:57:40 -0700

    Compress main function logic

--- check_msg.py
+++ check_msg.py
@@ -31,8 +31,26 @@ REQUIRED_TAGS = set(['[BUGFIX]', '[TASK]', '[FEATURE]'])
 OPTIONAL_TAGS = set(['[!!!]', '[API]', '[CONF]', '[DB]', '[SECURITY]'])
 
 
-def analyze_tags(line):
-    """Analyze tags.
+def analyze_file(commit_flname):
+    """Analyze contents from commit file.
+
+    Args:
+        commit_flname (str): Name of commit file.
+
+    Returns:
+        list: Lines inside commit file.
+    """
+    commit_buffer = []
+    with open(commit_flname, 'r') as fl:
+        for line in fl.readlines():
+            if not line.startswith('#'):
+                commit_buffer.append(line)
+    assert len(commit_buffer) > 0, 'Empty commit message'
+    return commit_buffer
+
+
+def analyze_tags(subject_line):
+    """Analyze tags from subject line.
 
     Args:
         line (str): Collection of words.
@@ -41,12 +59,13 @@ def analyze_tags(line):
         set: Required and optional tags.
 
     Raises:
-        AssertionError: Invalid tags or unmet contract for required tag.
+        AssertionError: Invalid subject line or bad tags.
     """
+    assert valid_line(subject_line, MAX_SLEN), 'Subject line too long'
+    subject_words = subject_line.split()
     invalid_tags = []
     required_tags = []
     optional_tags = []
-    subject_words = line.split()
     for word in subject_words:
         if word in REQUIRED_TAGS:
             required_tags.append(word)
@@ -54,8 +73,8 @@ def analyze_tags(line):
             optional_tags.append(word)
         elif word.startswith('[') and word.endswith(']'):
             invalid_tags.append(word)
-    assert len(invalid_tags) == 0, 'Invalid tags found'
-    assert len(required_tags) == 1, 'Please provide one required tag'
+        assert len(invalid_tags) == 0, 'Invalid tags found'
+        assert len(required_tags) == 1, 'Please provide one required tag'
     return set(required_tags + optional_tags) - set(['!!!'])
 
 
@@ -80,7 +99,7 @@ def analyze_lines(commit_buffer):
     return last_lines
 
 
-def analyze_labels(subject_tags, labels):
+def analyze_labels(subject_tags, last_lines):
     """Checks if labels are good.
 
     Args:
@@ -91,6 +110,9 @@ def analyze_labels(subject_tags, labels):
         KeyError: Invalid label(s).
         AssertionError: Invalid label tag(s).
     """
+    if '[TASK]' not in subject_tags:
+        assert len(last_lines) > 0, 'Required metadata tags'
+    labels = map(lambda line: line.split(':')[0], last_lines)
     label_tags = set()
     for label in labels:
         if label.lower().startswith('fixes #'):
@@ -129,23 +151,10 @@ def valid_metadata(val):
 
 def main():
     commit_flname = sys.argv[1]
-    commit_buffer = []
-
-    with open(commit_flname) as fl:
-        for line in fl.readlines():
-            if not line.startswith('#'):
-                commit_buffer.append(line)
-    assert len(commit_buffer) > 0, 'Empty commit message'
-
-    subject_line = commit_buffer.pop(0)
-    assert valid_line(subject_line, MAX_SLEN), 'Subject line too long'
-
-    subject_tags = analyze_tags(subject_line)
+    commit_buffer = analyze_file(commit_flname)
+    subject_tags = analyze_tags(commit_buffer.pop(0))
     last_lines = analyze_lines(commit_buffer)
-    if '[TASK]' not in subject_tags:
-        assert len(last_lines) > 0, 'Required metadata tags'
-    labels = map(lambda line: line.split(':')[0], last_lines)
-    analyze_labels(subject_tags, labels)
+    analyze_labels(subject_tags, last_lines)
 
 
 if __name__ == '__main__':
