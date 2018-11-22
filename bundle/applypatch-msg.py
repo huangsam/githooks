f43fad4ac1e35ec482f08e870d6824f9ee07a42c
Samuel Huang <samhuang91@gmail.com>
2018-11-22 14:37:14 -0800

    Fix import errors

--- bundle/applypatch-msg.py
+++ bundle/applypatch-msg.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import sys
 
-from githooks.core.message import check_message, check_non_master
+from githooks.core.message import check_message
+from githooks.core.branch import check_non_master
 
 commit_fl = sys.argv[1]
 
