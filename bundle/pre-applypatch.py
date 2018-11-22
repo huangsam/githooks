c06dd24d8580049aaf88788cdc8c8cd86273382e
Samuel Huang <samhuang91@gmail.com>
2018-11-22 09:11:18 -0800

    Fixup bundle and githooks

--- bundle/pre-applypatch.py
+++ bundle/pre-applypatch.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-import os
-
 from githooks.core.syntax import check_flake8
 
-repo_path = os.path.dirname(os.path.realpath(__name__))
-check_flake8(repo_path)
+check_flake8()
--- githooks/core/message.py
+++ githooks/core/message.py
@@ -1,8 +1,8 @@
 import githooks.analysis as a
 
 
-def check_message(filename):
-    commit_buffer = a.analyze_file(filename)
+def check_message(commit_fl):
+    commit_buffer = a.analyze_file(commit_fl)
     subject_tags = a.analyze_tags(commit_buffer.pop(0))
     last_lines = a.analyze_lines(commit_buffer)
     a.analyze_labels(subject_tags, last_lines)
