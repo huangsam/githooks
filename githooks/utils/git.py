75411b52f233a98d27d2edae4a5d27e6e94276e3
Samuel Huang <samhuang91@gmail.com>
2018-11-22 09:27:34 -0800

    Shorten git utility code

--- githooks/utils/git.py
+++ githooks/utils/git.py
@@ -7,16 +7,15 @@ from pygit2 import (
 
 
 def get_repo_from_cwd():
-    current_working_directory = os.getcwd()
-    repository_path = discover_repository(current_working_directory)
+    repository_path = discover_repository(os.getcwd())
     return Repository(repository_path)
 
 
 def get_branch_name(repo):
-    head_name = repo.head.name.replace('refs/heads/', '')
+    head_name = repo.head.shorthand
     return head_name.split('/')[-1]
 
 
 def get_branch_tag(repo):
-    head_name = repo.head.name.replace('refs/heads/', '')
+    head_name = repo.head.shorthand
     return head_name.split('/')[-2]
