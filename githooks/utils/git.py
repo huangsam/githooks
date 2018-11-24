f3efdade33bee40718010fa705fd5af8afd7c9ce
Samuel Huang <samhuang91@gmail.com>
2018-11-24 13:21:13 -0800

    Add composition of functions

--- githooks/utils/git.py
+++ githooks/utils/git.py
@@ -7,18 +7,18 @@ def _get_repo_from_cwd():
     return Repo(os.getcwd(), search_parent_directories=True)
 
 
+def _get_branch_name():
+    repo = _get_repo_from_cwd()
+    return repo.active_branch.name
+
+
 def get_branch_tag():
-    repo = _get_repo_from_cwd()
-    head_name = repo.active_branch.name
-    return head_name.split('/')[0]
+    return _get_branch_name().split('/')[0]
 
 
-def get_branch_name():
-    repo = _get_repo_from_cwd()
-    head_name = repo.active_branch.name
-    return head_name.split('/')[1]
+def get_branch_title():
+    return _get_branch_name().split('/')[1]
 
 
 def is_master_checked_out():
-    repo = _get_repo_from_cwd()
-    return repo.active_branch.name == 'master'
+    return _get_branch_name() == 'master'
